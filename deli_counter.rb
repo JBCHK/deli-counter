bc42e4f7a54bf50744bf354553977dfc735fc712
Jonathan Block <blockjonathan29@gmail.com>
2021-01-17 21:37:24 -0500

    commit tree 66e60bbc16ed60e5823d56278254c39a18f83532

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,32 @@
-# Write your code here.
+require "pry"
+
+def line (array)
+    joiner = []
+    counter = 1
+    if array.length == 0
+       puts "The line is currently empty."
+    else
+    array.each do |name| 
+    joiner.push("#{counter}. #{name}")
+    counter += 1
+    end
+    puts "The line is currently: #{joiner.join(' ')}"
+  end
+end
+
+
+def take_a_number(array, name)    
+        array.push(name)
+        puts "Welcome, #{name}. You are number #{array.length} in line."
+end
+
+def now_serving(array)       
+    if array.length == 0
+        puts "There is nobody waiting to be served!"
+    else
+        puts "Currently serving #{array.shift}."
+    end
+end
+
+
+
